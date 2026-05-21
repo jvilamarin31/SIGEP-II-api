fd9d0222f00814d962494b1f07cb58455cbd86d8
Jorge Andrés Medina <jorgeamedinau@gmail.com>
2026-05-20 19:36:37 -0500

    Update CORS configuration to use allowed origin patterns

--- backend/src/main/java/com/apirest/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/apirest/backend/config/SecurityConfig.java
@@ -65,7 +65,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://sigep-ii-api.vercel.app"));
+        configuration.setAllowedOriginPatterns(List.of(
+        "http://localhost:5173",
+        "https://*.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
